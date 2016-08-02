5a9ec1f693e16a0f1cddb6598709a80d8426164f
wigzard <nedolugin@gmail.com>
2016-08-02 17:34:59 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by User on 02.08.2016.
+ */
+public class MainClass {
+
+    public static  void main(String[] args){
+        System.out.println("Hello World!!!");
+
+        for(int i=0 ; i<10; ++i){
+            System.err.println("Ошибака");
+        }
+    }
+
+}
